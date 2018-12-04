05838e1d37fc7456c7a702931c443875ab177a9e
Nastya <duxwife@yandex.ru>
2018-12-04 18:05:57 +0800

    api готов

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,11 +38,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            'throttle:60,1',
+            'bindings',
         ],
+
     ];
 
     /**
